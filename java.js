7d0e1e1ecadf5930bdb86c3f27db732e89d6ed2f
BoonyaritThitipisut <137771110+BoonyaritThitipisut@users.noreply.github.com>
2023-08-28 16:21:56 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+
+
+
+function btn(){
+    let num = Math.floor(Math.random() * 6) + 1;
+    let num2 = Math.floor(Math.random() * 6) + 1;
+    let num3 = Math.floor(Math.random() * 6) + 1;
+console.log(num,num2,num3);
+}
